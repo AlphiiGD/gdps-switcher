c501c17199e4daba749ae86f2cc6d027066e0ce7
kam <100500023+Kingminer7@users.noreply.github.com>
2025-12-07 12:53:17 -0500

    last few changes

--- src/hooks/MusicDownloadManager.cpp
+++ src/hooks/MusicDownloadManager.cpp
@@ -8,25 +8,13 @@ using namespace geode::prelude;
 class $modify(GDPSMDL, MusicDownloadManager) {
 
     std::string pathForSFXFolder(int id) {
-        auto ret = MusicDownloadManager::pathForSFXFolder(id);
-        if (!GDPSMain::get()->isActive()) return ret;
-        log::info("{}", getGDPSPath(ret));
-        return getGDPSPath(ret);
+        if (!GDPSMain::get()->isActive()) return MusicDownloadManager::pathForSFXFolder(id);
+        auto server = GDPSMain::get()->m_servers[GDPSMain::get()->m_currentServer];
+        return string::pathToString(dirs::getSaveDir() / "gdpses" / server.saveDir / "");
     }
 
     std::string pathForSongFolder(int id) {
-        auto ret = MusicDownloadManager::pathForSongFolder(id);
-        if (!GDPSMain::get()->isActive()) return ret;
-        log::info("{}", getGDPSPath(ret));
-        return getGDPSPath(ret);
-    }
-
-    // cause gd::string
-    inline std::string getGDPSPath(const gd::string& path) {
-        return getGDPSPath(std::filesystem::path(path));
-    }
-
-    std::string getGDPSPath(const std::filesystem::path& orig) {
+        if (!GDPSMain::get()->isActive()) return MusicDownloadManager::pathForSongFolder(id);
         auto server = GDPSMain::get()->m_servers[GDPSMain::get()->m_currentServer];
         return string::pathToString(dirs::getSaveDir() / "gdpses" / server.saveDir / "");
     }
